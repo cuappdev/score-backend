0b5eeafd340043e57be51bb91bae665c174afd5d
Daisy Chang <dmc387@cornell.edu>
2024-11-02 22:09:08 -0400

    Added urls for women's sports

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -28,6 +28,9 @@ SCHEDULE_PREFIX = "https://cornellbigred.com/sports/"
 # The postfix for the sports schedules
 SCHEDULE_POSTFIX = "/schedule"
 
+# The tag for each result
+RESULT_TAG = ".sidearm-schedule-game-result"
+
 # The dictionary mapping sports urls to gender
 SPORT_URLS = {
     "baseball": {"sport": "Baseball", "gender": "Mens"},
@@ -47,4 +50,22 @@ SPORT_URLS = {
     "mens-tennis": {"sport": "Tennis", "gender": "Mens"},
     "mens-track-and-field": {"sport": "Track & Field", "gender": "Mens"},
     "wrestling": {"sport": "Wrestling", "gender": "Mens"},
+    "womens-basketball": {"sport": "Basketball", "gender": "Womens"},
+    "womens-cross-country": {"sport": "Cross Country", "gender": "Womens"},
+    "equestrian": {"sport": "Equestrian", "gender": "Womens"},
+    "fencing": {"sport": "Fencing", "gender": "Womens"},
+    "field-hockey": {"sport": "Field Hockey", "gender": "Womens"},
+    "womens-gymnastics": {"sport": "Gymnastics", "gender": "Womens"},
+    "womens-ice-hockey": {"sport": "Ice Hockey", "gender": "Womens"},
+    "womens-lacrosse": {"sport": "Lacrosse", "gender": "Womens"},
+    "womens-polo": {"sport": "Polo", "gender": "Womens"},
+    "womens-rowing": {"sport": "Rowing ", "gender": "Womens"},
+    "womens-sailing": {"sport": "Sailing", "gender": "Womens"},
+    "womens-soccer": {"sport": "Soccer", "gender": "Womens"},
+    "softball": {"sport": "Softball", "gender": "Womens"},
+    "womens-squash": {"sport": "Squash", "gender": "Womens"},
+    "womens-swimming-and-diving": {"sport": "Swimming & Diving", "gender": "Womens"},
+    "womens-tennis": {"sport": "Tennis", "gender": "Womens"},
+    "womens-track-and-field": {"sport": "Track & Field", "gender": "Womens"},
+    "womens-volleyball": {"sport": "Volleyball", "gender": "Womens"},
 }
