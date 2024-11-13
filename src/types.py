1f435bb5c1efb5aa478910803c67509b136be9f9
Daisy Chang <dmc387@cornell.edu>
2024-11-13 18:27:53 -0500

    Added queries by gender and sport

--- src/types.py
+++ src/types.py
@@ -77,8 +77,9 @@ class GameType(ObjectType):
         - `city`: The city of the game.
         - `date`: The date of the game.
         - `gender`: The gender of the game.
-        - `location`: The location of the game.
+        - `location`: The location of the game. (optional)
         - `opponent_id`: The id of the opposing team.
+        - `result`: The result of the game. (optional)
         - `sport`: The sport of the game.
         - `state`: The state of the game.
         - `box_score`: The box score of the game.
