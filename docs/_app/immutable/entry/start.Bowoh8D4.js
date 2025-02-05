4cdbcf378022064130f7fd90a323565023c49871
skyeslattery <dss354@cornell.edu>
2025-02-04 19:34:19 -0500

    Testing magidoc documentation

---
+++ docs/_app/immutable/entry/start.Bowoh8D4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DqZ6GuRm.js";export{t as start};
---
+++ magidoc.mjs
@@ -0,0 +1,9 @@
+export default {
+  introspection: {
+    type: 'url',
+    url: 'http://localhost:8000/graphql',
+  },
+  website: {
+    template: 'carbon-multi-page',
+  },
+}
