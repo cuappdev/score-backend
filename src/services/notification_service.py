acdced06e7b5b0ee34ad4acdcbc8536226f129d0
AnikDey-exe <deyanik2007@gmail.com>
2026-02-25 18:04:19 -0500

    added implementation comments for notifications

--- src/services/notification_service.py
+++ src/services/notification_service.py
@@ -29,4 +29,11 @@ class NotificationService:
             ),
             tokens=tokens,
         )
+
+        # create a token creation graphql mutation that accepts an fcm token and possibly a user id and timestamp
+        # occasionally remove tokens that are older than 30 days using a script that runs every day at midnight
+        # in the database associate the token with game ids/sports that the user has favorited
+        # in send_notfiication method, delete the token if it fails
+        # in the games scraping script, send notifications to the user for games that are upcoming and they have favorited by querying the tokens and using $in to check tokens that have the game id/sport
+        # might have to create device_id table that stores device id with current fcm token
         return messaging.send_each_for_multicast(message)
